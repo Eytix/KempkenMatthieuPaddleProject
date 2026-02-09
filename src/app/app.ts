7b4c718d7dbdd4b164b6b0fb500aadb9e8226161
Eytix <matthieu.kempken@gmail.com>
2026-02-09 18:10:24 +0100

    important views content

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,25 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Menu} from './views/layout/menu/menu';
+import {Header} from './views/layout/header/header';
+import {Content} from './views/layout/content/content';
+import {Footer} from './views/layout/footer/footer';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatNavList} from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Menu,
+    Header,
+    Content,
+    Footer,
+    MatSidenav,
+    MatToolbar,
+    MatNavList,
+    MatSidenavContent,
+    MatSidenavContainer,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
