7176b3aa0c01c4daceed1d60e14171d30d4dba5c
Eytix <120930011+Eytix@users.noreply.github.com>
2026-04-27 12:10:16 +0200

    Button correct : groundlist

--- src/app/views/home/home.ts
+++ src/app/views/home/home.ts
@@ -1,10 +1,15 @@
 import { Component,inject } from '@angular/core';
 import {Groundservice} from '../ground/groundservice';
+import {MatListItem} from '@angular/material/list';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatListItem,
+    RouterLink
+  ],
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
 })
