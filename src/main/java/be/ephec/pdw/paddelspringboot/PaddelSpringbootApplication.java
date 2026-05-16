e2f739308ab3782242132d5c60bd27a450623b9e
Eytix <120930011+Eytix@users.noreply.github.com>
2026-05-16 15:15:34 +0200

    Backend add on developp

---
+++ src/main/java/be/ephec/pdw/paddelspringboot/PaddelSpringbootApplication.java
@@ -0,0 +1,13 @@
+package be.ephec.pdw.paddelspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaddelSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaddelSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ephec/pdw/paddelspringboot/PaddelSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ephec.pdw.paddelspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaddelSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
